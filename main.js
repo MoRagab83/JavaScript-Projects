4527ed50f5c6f408889a54977dfd95099ea6d726
MoRagab83 <104938565+MoRagab83@users.noreply.github.com>
2023-01-15 19:48:01 +0000

    Add files via upload
    Assignment Submission # 1

---
+++ main.js
@@ -0,0 +1,12 @@
+document.write("Hello, World");
+window.alert("Hello, World");
+var A = "This is a string";//Assignment # 7//
+document.write(A);
+document.write("Mo told Zain,\"Its time to sleep")<br> //Assignment # 8// 
+document.write("\"be who you are and say what you feel," + " because those who mind don\'t matter and thoes who matter don't mind.\"" + "Dr.Seuss"); //Assignment # 9//
+var Family="The Starks", Dad = " Eddard", doughter = "Sansa" son = "Rob" ; 
+document.write(Dad); //Assignment # 10// 
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues); //Assignment # 11// 
+(3+3) //Assignment # 12// 
