bc4ae46a6459bc7a4b7613de4541bf0af58fb7dd
MoRagab83 <104938565+MoRagab83@users.noreply.github.com>
2023-01-21 15:51:20 +0000

    under Assignment Submission # 5
    under Assignment Submission # 5

---
+++ main_2.js
@@ -0,0 +1,37 @@
+document.write(typeof "Hello"); // typeof operator // 
+
+function my_function() {
+    document.getElementById("Test").innerHTML = isNaN("This is a string"); // expression with a string //
+
+}
+
+function my_function()  { 
+    document.getElementById("Test").innerHTML = isNaN("007"); // expression with number //
+}
+
+document.write(2E320); // to get an infinity number // 
+
+document.write(-3E310); // to get a nigative infinity number // 
+
+document.write(10>2); // to get boolean value // 
+
+console.log(2+2) // to display results of math operation in the console // 
+
+document.write("10" + 5); // type coercion //
+
+document.write(10==10); // to check whether the data on the left side of the symbol is equal to the data on the right side //
+
+x = 10;
+y = 10;
+document.write(x === y); //  to show that a comparison should be made //
+
+document.write(5>2 && 10>4); // && operator determines the logic between values or variables //
+
+document.write(5>10 || 10>4); // || operator to returm true or false //
+
+function not_function()  {
+    document.getElementById("Not").innerHTML = !(5>10);
+}
+
+
+
